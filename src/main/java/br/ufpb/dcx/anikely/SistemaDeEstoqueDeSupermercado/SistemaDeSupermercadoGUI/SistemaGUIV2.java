53ef8c15172eae0a2e56d8da38845635385484c6
ANIKELY <anikelykel@gmail.com>
2026-02-11 16:52:35 -0300

    Adicionando o SistemaGUIV3.java

--- src/main/java/br/ufpb/dcx/anikely/SistemaDeEstoqueDeSupermercado/SistemaDeSupermercadoGUI/SistemaGUIV2.java
+++ src/main/java/br/ufpb/dcx/anikely/SistemaDeEstoqueDeSupermercado/SistemaDeSupermercadoGUI/SistemaGUIV2.java
@@ -5,5 +5,24 @@ import java.awt.*;
 
 public class SistemaGUIV2 extends JFrame {
     JLabel linha1,linha2;
-    ImagemIcon EstoqueImg = new ImageIcon("./imgs/EtoqueImg.jpg");
+    ImageIcon EstoqueImg = new ImageIcon("./imgs/EtoqueImg.jpg");
+    public SistemaGUIV2(){
+        setTitle("Estoque de Supermercado");
+        setSize(400,400);
+        setLocation(0,9);
+        setResizable(true);
+        getContentPane() .setBackground(Color.blue);
+        linha1 = new JLabel("Meu Sistema Estoque de Supermercado", JLabel.CENTER);
+        linha1.setForeground(Color.CYAN);
+        linha1.setFont(new Font("Serif",Font.BOLD,46 ));
+        linha2 = new JLabel(EstoqueImg, JLabel.CENTER);
+        getContentPane().setLayout(new GridLayout());
+        getContentPane().add(linha1);
+        getContentPane().add(linha2);
+    }
+    static void main(String[] args){
+        JFrame janela = new SistemaGUIV2();
+        janela.setVisible(true);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
---
+++ src/main/java/br/ufpb/dcx/anikely/SistemaDeEstoqueDeSupermercado/SistemaDeSupermercadoGUI/SistemaGUIV3.java
@@ -0,0 +1,11 @@
+package br.ufpb.dcx.anikely.SistemaDeEstoqueDeSupermercado.SistemaDeSupermercadoGUI;
+import javax.swing.*;
+
+public class SistemaGUIV3 extends JFrame {
+    JLabel linha1, linha2;
+    ImageIcon EstoqueImg = new ImageIcon("./imgs/Estoque.img");
+    ImageIcon addBusca = new ImageIcon ("");//Icone de busca
+    ImageIcon addList = new ImageIcon("");// Icone de Listar compras
+    ImageIcon addRemove = new ImageIcon(""); // Icone de remover compras
+
+}
