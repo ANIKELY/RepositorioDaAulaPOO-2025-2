47c81b40cd5a679a760c351c1b18136348d702d7
ANIKELY <anikelykel@gmail.com>
2026-03-31 08:07:17 -0300

    atualizando projetos

--- src/main/java/br/ufpb/dcx/anikely/SistemaDeEstoqueDeSupermercado/SistemaDeSupermercadoGUI/SistemaLoginGUI.java
+++ src/main/java/br/ufpb/dcx/anikely/SistemaDeEstoqueDeSupermercado/SistemaDeSupermercadoGUI/SistemaLoginGUI.java
@@ -17,12 +17,14 @@ public class SistemaLoginGUI extends JFrame{
     JButton adicionarUsuario;
     JButton removerUsuario;
     ImageIcon loginImg = new ImageIcon(new ImageIcon("./imgs/IconUser.png").getImage().getScaledInstance(400,200, Image.SCALE_SMOOTH));
-
+    ImageIcon enterImg = new ImageIcon(new ImageIcon("./imgs/IconEnter.png").getImage().getScaledInstance(250,200,Image.SCALE_SMOOTH));
+    ImageIcon addFuncionario = new ImageIcon(new ImageIcon("./imgs/IconFuncionario.png").getImage().getScaledInstance(250,200,Image.SCALE_SMOOTH));
+    ImageIcon removeFuncionario = new ImageIcon(new ImageIcon("./imgs/IconRemoveUsuario.png").getImage().getScaledInstance(250,200,Image.SCALE_SMOOTH));
     public SistemaLoginGUI(MeuSistemaDeEstoqueDeSupermercadoMap sistema){
         this.sistema = sistema;
 
         setTitle("Login");
-        setSize(700,600);
+        setSize(900,700);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
 
@@ -30,12 +32,19 @@ public class SistemaLoginGUI extends JFrame{
 
         usuarioLogin = new JTextField(15);
         senhaLogin = new JPasswordField(15);
-        entrar = new JButton("Entrar");
+        entrar = new JButton(enterImg);
         entrar.addActionListener(new SistemaLoginController(sistema,this, usuarioLogin, senhaLogin));
-        adicionarUsuario = new JButton("Adicionar novo usuário");
+        entrar.setBackground(Color.BLACK);
+        entrar.setBorderPainted(false);
+        adicionarUsuario = new JButton(addFuncionario);
+        adicionarUsuario.setBackground(Color.BLACK);
+        adicionarUsuario.setBorderPainted(false);
         adicionarUsuario.addActionListener(new SistemaAddFuncionarioController(sistema,this));
-        removerUsuario = new JButton("Remover usuário");
+        removerUsuario = new JButton(removeFuncionario);
+        removerUsuario.setBackground(Color.BLACK);
+        removerUsuario.setBorderPainted(false);
         removerUsuario.addActionListener(new SistemaRemoveFuncionarioController(sistema,this));
+
         getContentPane().setBackground(Color.BLACK);
 
         linha1 = new JLabel("Usuário: ");
@@ -59,7 +68,7 @@ public class SistemaLoginGUI extends JFrame{
         botoes.add(adicionarUsuario);
         botoes.add(removerUsuario);
 
-        JPanel panelCentral = new JPanel(new  GridLayout(2,1,10,10));
+        JPanel panelCentral = new JPanel(new  GridLayout(4,1));
         panelCentral.setBackground(Color.BLACK);
         panelCentral.add(panelUsuario);
         panelCentral.add(panelSenha);
